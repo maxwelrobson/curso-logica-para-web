0bf50c20b943b064143521f8e21fd937e13d8dc3
maxwelrobson <maxps2@live.com>
2026-06-22 11:07:29 -0300

    Exemplos - for while - for

---
+++ Módulo3_EstruturaDeRepetição/exemplo11.js
@@ -0,0 +1,10 @@
+/*Suponha um
+algoritmo que leia e
+escreva os números
+impares de 1 a 100.*/
+
+for (let index = 1; index <= 100; index++) {
+    if (index % 2 != 0) {
+        console.log(index)
+    }
+}
