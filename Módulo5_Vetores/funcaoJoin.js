0473b99bb94543e4f87f29589fedbd1fd3f5e163
maxwelrobson <maxps2@live.com>
2026-07-07 11:03:06 -0300

    Exemplo da função Join

--- Módulo5_Vetores/funcaoJoin.js
+++ Módulo5_Vetores/funcaoJoin.js
@@ -1,3 +1,7 @@
 var vetor1 = new Array(1, 2, 3, 4, 5);
+var elementos = vetor1.join();
+console.log(elementos);
+
+var vetor1 = new Array(1, 2, 3, 4, 5);
 var elementos = vetor1.join("-");
 console.log(elementos);
